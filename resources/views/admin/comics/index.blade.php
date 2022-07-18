c7dc5788de06d0ab4d86ed2c52279ed5fc45a62a
DanieleKun <pucciodaniele95@gmail.com>
2022-07-18 19:59:50 +0200

    add home page admin

---
+++ resources/views/admin/comics/index.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.templates.base')
+
+@section('mainContent')
+    <main>
+
+        <h2>Home Page Admin</h2>
+
+    </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PagesController@home')->name('home');
+Route::resource('/comics', 'ComicsController');
